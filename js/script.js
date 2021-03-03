e3c6ad7706d86bac7016156be85279a47f748df1
Henry Coleman <henrycoleman@henrys-mbp.lan>
2021-03-03 15:03:38 -0600

    progress1

--- js/script.js
+++ js/script.js
@@ -1,81 +1,11 @@
+// const url = "https://cdn.contentful.com/spaces/6snrh18ago17/environments/master/entries?access_token=SN8hehCRubh8DraZQsdjYFI7ZsHCsGfr5JrTRNwvrjc&content_type=triviaQ"
+// $.ajax(url)
+// .then((data)=> {
+//  console.log (data)
+// })
 
-let apiKey = {b671e2d2328c4c51bb811b72df6384bc}
 
-var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2021-02-25&' +
-          'sortBy=popularity&' +
-          'apiKey=var url = 'https://newsapi.org/v2/everything?' +
-          'q=Apple&' +
-          'from=2021-02-25&' +
-          'sortBy=popularity&' +
-          'apiKey=b671e2d2328c4c51bb811b72df6384bc';
+// ! APP STATE
 
-var req = new Request(url);
 
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })';
 
-var req = new Request(url);
-
-fetch(req)
-    .then(function(response) {
-        console.log(response.json());
-    })
-
-const NewsAPI = require('newsapi');
-const newsapi = new NewsAPI('API_KEY');
-// To query /v2/top-headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
-newsapi.v2.topHeadlines({
-  sources: 'bbc-news,the-verge',
-  q: 'bitcoin',
-  category: 'business',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query /v2/everything
-// You must include at least one q, source, or domain
-newsapi.v2.everything({
-  q: 'bitcoin',
-  sources: 'bbc-news,the-verge',
-  domains: 'bbc.co.uk, techcrunch.com',
-  from: '2017-12-01',
-  to: '2017-12-12',
-  language: 'en',
-  sortBy: 'relevancy',
-  page: 2
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      articles: [...]
-    }
-  */
-});
-// To query sources
-// All options are optional
-newsapi.v2.sources({
-  category: 'technology',
-  language: 'en',
-  country: 'us'
-}).then(response => {
-  console.log(response);
-  /*
-    {
-      status: "ok",
-      sources: [...]
-    }
-  */
-});
